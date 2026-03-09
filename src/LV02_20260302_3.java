92162768737f42e529af602301835459d9cfaddc
Saidfiasal Khawarin <saidfiasalkhawarin@gmail.com>
2026-03-09 10:19:12 +0100

    Anpassung Attribut von Rechteck wegen Erstellung von ENUM Farbe

--- src/LV02_20260302_3.java
+++ src/LV02_20260302_3.java
@@ -1,7 +1,7 @@
 public class LV02_20260302_3 {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         Position myPosition = new Position(5,7);
-        Rechteck myRechteck = new Rechteck(12,15, 6, 8);
+        Rechteck myRechteck = new Rechteck(12,15, 6, 8, Farbe.blau);
 
         System.out.println(myPosition);
         System.out.println(myRechteck);
