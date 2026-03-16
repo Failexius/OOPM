ac2820a7a6ee62f58e549e7f2285a2e3f1341a73
MinasEskander <wi25b024@technikum-wien.at>
2026-03-16 10:11:23 +0100

    Update LV01_20260223.java

--- src/LV01_20260223.java
+++ src/LV01_20260223.java
@@ -12,18 +12,7 @@ public class LV01_20260223 {
         grossesRechteck.setLaenge(14);
         grossesRechteck.breite = 9;
 
-        System.out.printf("kleinesRechteck(l=%d, b=%d, u=%d, f=%d)\n",kleinesRechteck.getLaenge(), kleinesRechteck.breite, (kleinesRechteck.breite + kleinesRechteck.berechneFlaeche()) * 2,kleinesRechteck.getLaenge() * kleinesRechteck.breite);
-        System.out.printf("grossesRechteck(l=%d, b=%d, u=%d, f=%d)\n", grossesRechteck.getLaenge(), grossesRechteck.breite, (grossesRechteck.getLaenge() + grossesRechteck.breite) * 2, grossesRechteck.getLaenge() * grossesRechteck.breite);
-
-        System.out.printf("kleinesRechteck(l=%d, b=%d, u=%d, f=%d)\n",kleinesRechteck.getLaenge(), kleinesRechteck.breite, kleinesRechteck.berechneUmfang(),kleinesRechteck.berechneFlaeche());
-        System.out.printf("grossesRechteck(l=%d, b=%d, u=%d, f=%d)\n", grossesRechteck.getLaenge(), grossesRechteck.breite, grossesRechteck.berechneUmfang(), grossesRechteck.berechneFlaeche());
-
-        //kleinesRechteck.laenge = 5;
-        kleinesRechteck.setLaenge(5);
-        System.out.printf("kleinesRechteck(l=%d, b=%d, u=%d, f=%d)\n",kleinesRechteck.getLaenge(), kleinesRechteck.breite, (kleinesRechteck.breite + kleinesRechteck.getLaenge()) * 2,kleinesRechteck.getLaenge() * kleinesRechteck.breite);
-        //kleinesRechteck.laenge = -5;
-        kleinesRechteck.setLaenge(-5);
-        System.out.printf("kleinesRechteck(l=%d, b=%d, u=%d, f=%d)\n",kleinesRechteck.getLaenge(), kleinesRechteck.breite, (kleinesRechteck.breite + kleinesRechteck.getLaenge()) * 2, kleinesRechteck.getLaenge() * kleinesRechteck.breite);
+      leinesRechteck.getLaenge(), kleinesRechteck.breite, (kleinesRechteck.breite + kleinesRechteck.getLaenge()) * 2, kleinesRechteck.getLaenge() * kleinesRechteck.breite);
 
         System.out.println();
         System.out.println(kleinesRechteck);
